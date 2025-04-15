3e9165ee2d58e174b3e674d116f49f09bc5e9381
Anwar Ansari <136564629+Anwar1094@users.noreply.github.com>
2025-04-16 00:55:44 +0530

    Update home.js

--- Home/home.js
+++ Home/home.js
@@ -1,5 +1,5 @@
 const host = 'https://hostelattendance-backend.onrender.com'
-const Pyhost = 'http://127.0.0.1:5000'
+const Pyhost = 'https://rock-becoming-startup-rock.trycloudflare.com/'
 
 const CountryObj = {
     "Afghanistan": [
